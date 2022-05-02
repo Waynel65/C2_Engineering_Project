aa2f905fbdb4a48dea8d89a6a7e50f03cb223362
mjqPauli <mjqpauli@bu.edu>
2022-05-02 00:25:56 -0400

    added dependency for donut

--- C2/make_shellcode.py
+++ C2/make_shellcode.py
@@ -1,13 +1,17 @@
-import donut
 import sys
+import 
 
 def main():
     filepath = sys.argv[1]
     
+    print("before donut")
     shellcode = donut.create(
         file = filepath,
-        method = "exec"
+        method = "_Z14IAmAGoodNoodlev",
     )
 
+    print(shellcode)
+
 if __name__ == "__main__":
+    print("before main")
     main()
